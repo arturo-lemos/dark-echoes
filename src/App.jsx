832b86a9bfd663189aef0ab588b41fd54ad85f33
Arturo <lemosart0987@gmail.com>
2025-09-30 19:11:02 -0500

    Added import {useState} and function App() boiler plate into app.jsx. Nothing on the page when I npm run dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { episodeList } from "./data.js";
+import { useState } from "react";
+
+// I verified that the import was pulling the array and objects from data.js
+// console.log(episodeList);
+
+function App() {
+  const [recipeList] = useState(episodeList);
+
+  return (
+    <>
+      <header>
+        <h1>Episode List</h1>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
 export default function App() {
   // TODO
 }
